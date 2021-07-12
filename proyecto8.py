8e66a2f8856d35e3d5e87778ff2f023e732a80bf
juanpablopaez <86994240+juanpablopaez@users.noreply.github.com>
2021-07-12 00:24:45 -0400

    Update proyecto8.py

--- proyecto8.py
+++ proyecto8.py
@@ -1,3 +1,4 @@
+##proyecto 8 Javier Rivera,Juan Pablo Paez 
 import matplotlib.pyplot as plt
 import pandas as pd
 x=pd.read_csv('https://raw.githubusercontent.com/MinCiencia/Datos-COVID19/master/output/producto27/CasosNuevosSinSintomas.csv')
